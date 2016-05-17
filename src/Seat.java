5208e90524e3ca02db6a89dd287c07886d08c4c7
cwade12c <cwade12c@gmail.com>
2016-05-17 00:17:13 -0700

    Added method sigs
    Added method signatures to the Seat.java class.

--- src/Seat.java
+++ src/Seat.java
@@ -1,5 +1,22 @@
-/**
- * Created by shawnkoon on 5/16/2016.
- */
-public class Seat {
+public class Seat 
+{
+   private int row;
+   private char col;
+   private boolean isBooked;
+   
+   public Seat(int row, char col)
+   {
+      this.row = row;
+      this.col = col;
+   }
+   
+   private boolean getStatus()
+   {
+      return this.isBooked;
+   }
+   
+   private boolean setStatus(boolean flag)
+   {
+      this.isBooked = flag;
+   }
 }
