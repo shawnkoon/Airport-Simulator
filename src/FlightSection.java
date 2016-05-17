fbfb6af94042f6e9c08341d11e3414f6f2dd176e
cwade12c <cwade12c@gmail.com>
2016-05-17 00:27:25 -0700

    Added method sigs
    Added method signatures to FlightSection.java

--- src/FlightSection.java
+++ src/FlightSection.java
@@ -1,5 +1,29 @@
-/**
- * Created by shawnkoon on 5/16/2016.
- */
-public class FlightSection {
+public class FlightSection 
+{
+   private String flightClass;
+   private boolean[][] seats;
+   private boolean openSeats;
+   
+   public FlightSection(String flightClass, int row, int col)
+   {
+      this.flightClass = flightClass;
+      this.seats = new int[row][col];
+      this.openSeats = true;
+   }
+   
+   public void bookSeat(int row, int col)
+   {
+      this.seats[row][col] = true;
+      
+      /*if(seatsAreFull)
+      {
+         this.openSeats = false;
+      }
+      */
+   }
+   
+   public boolean hasAvailableSeats()
+   {
+      return this.openSeats;
+   }
 }
