aaaeac042f93e46e9bac48cbd04817fdf29fd4c4
cwade12c <cwade12c@gmail.com>
2016-05-21 02:30:01 -0700

    Made Company abstract again. We actually will not instantiate Company itself, but its children polymorphically.

--- src/Company.java
+++ src/Company.java
@@ -1,6 +1,6 @@
 // This class maintains information about airlines. An airline can have 0 or more flights associated with it.
 // When created an airline is not associated with any flights. All flights for a given airline must have unique flight ids.
-public class Company
+public abstract class Company
 {
     private String name;
 
