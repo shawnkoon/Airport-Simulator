149581709071f2fa974193604cdbec8e729cd1f3
cwade12c <cwade12c@gmail.com>
2016-05-21 01:50:08 -0700

    We may need to instantiate a Company in the future, so removing the "abstract" keyword for now.

--- src/Company.java
+++ src/Company.java
@@ -1,6 +1,6 @@
 // This class maintains information about airlines. An airline can have 0 or more flights associated with it.
 // When created an airline is not associated with any flights. All flights for a given airline must have unique flight ids.
-public abstract class Company
+public class Company
 {
     private String name;
 
