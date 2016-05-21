5a0db78ed5bb4920e580d105366a59e1bed27e2f
cwade12c <cwade12c@gmail.com>
2016-05-21 02:10:31 -0700

    Created abstract Path class to represent the different travel trips (a flight trip, a boat trip, or a train trip).

---
+++ src/Path.java
@@ -0,0 +1,21 @@
+public abstract class Path
+{
+    private String company;
+    private String depart;
+    private String destination;
+    private int year;
+    private int month;
+    private int day;
+    private String ticketID;
+
+    public Path(String company, String depart, String destination, int year, int month, int day, String ticketID)
+    {
+        this.company = company;
+        this.depart = depart;
+        this.destination = destination;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.ticketID = ticketID;
+    }
+}
