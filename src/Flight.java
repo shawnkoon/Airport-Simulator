5bac80cc8af444ab1ebbcd4b1d49e9629bd95eeb
cwade12c <cwade12c@gmail.com>
2016-05-21 02:11:38 -0700

    Moved attributes to parent class and make a call to super's constructor.

--- src/Flight.java
+++ src/Flight.java
@@ -1,22 +1,7 @@
-public class Flight {
-
-    private String name;
-    private String orig;
-    private String dest;
-    private int year;
-    private int month;
-    private int day;
-    private String id;
-
-    public Flight(String name, String orig, String dest, int year, int month, int day, String id)
+public class Flight extends Path
+{
+    public Flight(String company, String depart, String destination, int year, int month, int day, String ticketID)
     {
-        this.name = name;
-        this.orig = orig;
-        this.dest = dest;
-        this.year = year;
-        this.month = month;
-        this.day = day;
-        this.id = id;
+        super(company, depart, destination, year, month, day, ticketID);
     }
-
 }
