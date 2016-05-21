68eed873a1cf2aa95789091ecfed8ef47b2dadc2
cwade12c <cwade12c@gmail.com>
2016-05-21 00:44:09 -0700

    Made Airport constructor public so that it can be created with an AirportFactory. Moved createAirport() method to appropriate AirportFactory class.

--- src/Airport.java
+++ src/Airport.java
@@ -3,24 +3,12 @@
  */
 // Objects of this class represent airports.
 // The only information maintained is the name, which must be exactly 3 characters in length.
-public class Airport {
+public class Airport
+{
     private String name;
 
-    private Airport(String name)
+    public Airport(String name)
     {
         this.name = name;
-    }
-
-    // Creates Airport object only if name is == 3 characters long.
-    public Airport createAirport(String name)
-    {
-        if(name.length() == 3)
-        {
-            return new Airport(name);
-        }
-        else
-        {
-            return null;
-        }
     }
 }
--- src/AirportFactory.java
+++ src/AirportFactory.java
@@ -1,4 +1,20 @@
 public class AirportFactory implements TransportationFactory
 {
-   
+    public AirportFactory()
+    {
+
+    }
+
+    // Creates Airport object only if name is == 3 characters long.
+    public Airport createAirport(String name)
+    {
+        if(name.length() == 3)
+        {
+            return new Airport(name);
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
