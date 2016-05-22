69019bd75c74eef068073707c486bbcd66fefe55
Kang Son <shawn9405@gmail.com>
2016-05-22 10:41:01 -0700

    TransportFact : package private -> public

--- src/TransportationFactory.java
+++ src/TransportationFactory.java
@@ -1,21 +1,21 @@
 public interface TransportationFactory
 {
    //method sig to create a boarding station
-    Transport createTransport(String name);
+   public Transport createTransport(String name);
    
    //method sig to create travel company (i.e. Delta, Royal, etc.)
-    Company createCompany(String name);
+   public Company createCompany(String name);
    
    //method sig to create a section
-    Section createSection(String company, String ticketID, int row, int col, String seatClass);
+   public Section createSection(String company, String ticketID, int row, int col, String seatClass);
 
    //method sig to create a travel path (FROM - TO)
-    Path createPath(String company, String depart, String destination, int year, int month, int day, String ticketID);
+   public Path createPath(String company, String depart, String destination, int year, int month, int day, String ticketID);
    
    //method sig to display system details
-    void displaySystemDetails();
+   public void displaySystemDetails();
    
    //method sig to find all available seats
-    void findAvailableSeats(String depart, String destination);
+   public void findAvailableSeats(String depart, String destination);
 
 }
