7b325ad1cd53e03a5a3bf02ec5522de17f9fa01f
-wade <cwade12c@gmail.com>
2016-06-08 15:04:34 -0700

    Change seat class pricing specifically
    Change the seat class (e.g., economy) pricing for an origin and destination for a given airline.

--- src/SampleClient.java
+++ src/SampleClient.java
@@ -18,6 +18,176 @@ public class SampleClient {
 
         SystemManager res = new SystemManager();
 
+        res.createAirport("DEN");
+        res.createAirport("DFW");
+        res.createAirport("LON");
+        res.createAirport("DEN");//invalid
+        res.createAirport("DENW");//invalid
+
+        System.out.println("\n=====About to create Airlines.=====\n");
+
+        res.createAirline("DELTA");
+        res.createAirline("AMER");
+        res.createAirline("FRONT");
+        res.createAirline("FRONTIER"); //invalid
+        res.createAirline("FRONT"); //invalid
+
+        System.out.println("\n=====About to create Flights.=====\n");
+
+        res.createFlight("DELTA", "DEN", "LON", 2016, 10, 10, "123");
+        res.createFlight("DELTA", "DEN", "LON", 2012, 1, 20, "123");//invalid id.
+        res.createFlight("DELTA", "DEN", "DFW", 2016, 8, 8, "567abc");
+        res.createFlight("FRONT", "DFW", "DEN", 2016, 10, 10, "123");
+        res.createFlight("AMER", "DEN", "LON", 2016, 2, 15, "123");
+        res.createFlight("AMER", "DEN", "LON", 2016, 2, 30, "12322");
+        res.createFlight("DEL", "DEN", "LON", 2013, 9, 8, "567"); //invalid airline
+        res.createFlight("DELTA", "LON33", "DEN33", 2013, 5, 7, "123");//invalid airports
+        res.createFlight("AMER", "DEN", "LON", 2010, 40, 100, "123abc");//invalid date
+
+        System.out.println("\n=====About to create Section.=====\n");
+
+        res.createSection("DELTA","123", 's', 2, SeatClass.economy, 100.00); //s layout
+        //res.createSection("DELTA","123", 2, 2, SeatClass.economy);
+        res.createSection("DELTA","123", 'm', 3, SeatClass.first, 250.00);
+        res.createSection("DELTA","123", 2, 3, SeatClass.first);//Invalid seat,
+        res.createSection("DELTA","123", 'w', 7, SeatClass.business, 500.00);
+        res.createSection("SWSERTT","123", 5, 5, SeatClass.economy);//Invalid airline
+        res.createSection("FRONT", "123", 6, 2, SeatClass.economy);
+        res.createSection("DELTA", "567abc", 2, 2, SeatClass.economy);
+        res.createSection("AMER", "12", 9, 9, SeatClass.first);//Invalid ID.
+
+        System.out.println("\n=====About to print Available Flights.=====\n");
+
+        res.findAvailableFlights("DEN", "LON");
+
+        System.out.println("\n=====About to book some seats.=====\n");
+
+        res.bookSeat("DELTA", "123", SeatClass.first, 1, 'A');
+        //res.bookSeat("DELTA", "123", SeatClass.economy, 1, 'A');
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.economy, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        System.out.println("M should be full....");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.first, "aisle");
+        //col c will be filled
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col d will be filled
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col a is next available in this section
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col b...
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col e
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col f
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col g
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col h
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col i
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        //col j
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+        res.bookSeat("DELTA", "123", SeatClass.business, "aisle");
+
+        System.out.println("Layout w should be full......................");
+        res.bookSeat("DELTA", "123", SeatClass.business, "window");
+        res.bookSeat("DELTA", "123", SeatClass.business, "window");
+
+
+        //res.bookSeat("DELTA", "123", SeatClass.economy, 1, 'B');
+        res.bookSeat("DELTA888", "123", SeatClass.business, 1, 'A'); //Invalid airline
+        res.bookSeat("DELTA", "123haha7", SeatClass.business, 1, 'A'); //Invalid flightId
+        //res.bookSeat("DELTA", "123", SeatClass.economy, 1, 'A'); //
+
+        // already booked
+
+        res.displaySystemDetails();
+
+        System.out.println("\n\n" + Layout.SMALL.getValue());
+
+        res.changePriceSeats(SeatClass.economy.toString(), 50000.0);
+
+        res.displaySystemDetails();
+
         while(choice != 9)
         {
             // 1. Create an airport system by using information provided in an input file.
@@ -132,6 +302,168 @@ public class SampleClient {
             }
             else if(choice == 4)
             {
+                String airline = "";
+                boolean check = false;
+
+
+                while(check == false)
+                {
+                    try
+                    {
+                        Scanner kb = new Scanner(System.in);
+                        System.out.println("Please enter an airline : ");
+                        airline = kb.nextLine();
+
+                        if (res.hasAirline(airline))
+                        {
+                            check = true;
+                        }
+                        else
+                        {
+                            System.out.println("Airline not found!");
+                        }
+                    }
+                    catch(Exception e)
+                    {
+
+                    }
+                }
+
+                String origin = "";
+                check = false;
+
+                while(check == false)
+                {
+                    try
+                    {
+                        Scanner kb = new Scanner(System.in);
+                        System.out.println("Please enter an origin : ");
+                        origin = kb.nextLine();
+
+                        if (res.hasAirport(origin))
+                        {
+                            check = true;
+                        }
+                        else
+                        {
+                            System.out.println("Origin not found!");
+                        }
+                    }
+                    catch(Exception e)
+                    {
+
+                    }
+                }
+
+                String destination = "";
+                check = false;
+
+                while(check == false)
+                {
+                    try
+                    {
+                        Scanner kb = new Scanner(System.in);
+                        System.out.println("Please enter a destination : ");
+                        destination = kb.nextLine();
+
+                        if (res.hasAirport(destination))
+                        {
+                            check = true;
+                        }
+                        else
+                        {
+                            System.out.println("Destination not found!");
+                        }
+                    }
+                    catch(Exception e)
+                    {
+
+                    }
+                }
+
+
+
+                System.out.println("\nPlease choose a Seat Class to update:\n\t1. ECONOMY\n\t2. FIRST\n\t3. BUSINESS\n\t4. QUIT");
+                System.out.print("Please enter your choice : ");
+
+                check = false;
+                int result = -1;
+
+                while(check == false)
+                {
+                    try
+                    {
+                        Scanner kb = new Scanner(System.in);
+                        result = kb.nextInt();
+
+                        if(result >= 1 && result <= 4)
+                        {
+                            check = true;
+                        }
+                    }
+                    catch(Exception e)
+                    {
+
+                    }
+
+                    if(check == false)
+                    {
+                        System.out.print("Please enter a number between [1-3] : ");
+                    }
+                }
+
+                if(result != 4)
+                {
+                    String seatClass = "";
+
+                    switch(result)
+                    {
+                        case 1:
+                            seatClass = "ECONOMY";
+                            break;
+                        case 2:
+                            seatClass = "FIRST";
+                            break;
+                        case 3:
+                            seatClass = "BUSINESS";
+                            break;
+                    }
+
+                    check = false;
+                    double seatPrice = 0.0;
+
+                    while(check == false)
+                    {
+                        System.out.print("\nPlease enter a new price or Q to quit : ");
+
+                        try
+                        {
+                            Scanner kb = new Scanner(System.in);
+                            String price = kb.nextLine();
+
+                            if(!price.toUpperCase().equals("Q"))
+                            {
+                                seatPrice = Double.parseDouble(price);
+                                check = true;
+                            }
+                            else
+                            {
+                                check = true;
+                            }
+
+                        }
+                        catch(Exception e)
+                        {
+
+                        }
+                    }
+
+                    if(!seatClass.equals("") && seatPrice != 0.0)
+                    {
+                        res.changePriceSeat(airline, origin, destination, seatClass, seatPrice);
+                        System.out.println("All flights from [" + origin + "] to [" + destination + "] at airline [" + airline + "] in a seat class of [" + seatClass + "] have been updated to: $" + seatPrice);
+                    }
+                }
 
             }
             else if(choice == 5)
